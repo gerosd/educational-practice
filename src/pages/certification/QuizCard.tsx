4f350ad5c61e5244e1c9b4efb43afcb27b97bbd9
Влад222 <qabarma@example.com>
2025-05-06 11:21:52 +0300

    Update

--- src/pages/certification/QuizCard.tsx
+++ src/pages/certification/QuizCard.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 
+interface QuizTests {
+   id: number;
+   question: string;
+   code: "";
+   options: [];
+   answer: number;
+}
+
 interface QuizProps {
     styles: Record<string, string>;
 }
+
+const quizTestsArray: QuizTests[] = [
+    {id: 1, question: "Как правильно создать компонент React?", code: '', options: ['function Component() { return <div>Hello</div>; }', ''], answer: 1}
+]
 
 function QuizCard( {styles}: QuizProps ): React.ReactElement {
     return (
